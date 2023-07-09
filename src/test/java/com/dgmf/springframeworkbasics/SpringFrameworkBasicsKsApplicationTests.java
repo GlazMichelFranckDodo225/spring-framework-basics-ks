7bdeb9fc3a40595864b50d463296e6800228efec
FranckDodo91 <devlogappdgmf@gmail.com>
2023-07-09 09:08:43 +0200

    Project initialization

---
+++ src/test/java/com/dgmf/springframeworkbasics/SpringFrameworkBasicsKsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.springframeworkbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkBasicsKsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
